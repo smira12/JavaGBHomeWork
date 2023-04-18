0e3ac164ca375478df34d15f2a553c1a3d2be1a4
smira12 <m1ronsa89@gmail.com>
2023-04-18 09:25:56 +0300

    my Task 4

---
+++ HomeWork4/Task2.java
@@ -0,0 +1,44 @@
+package HomeWork4;
+
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+//         enqueue() - помещает элемент в конец очереди,
+//         dequeue() - возвращает первый элемент из очереди и удаляет его,
+//         first() - возвращает первый элемент из очереди, не удаляя
+
+import java.util.LinkedList;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> dq = new LinkedList<>();
+
+        enqueue(dq, 1);
+        enqueue(dq, 2);
+        enqueue(dq, 3);
+        enqueue(dq, 4);
+        enqueue(dq, 5);
+        System.out.println(dq);
+
+        System.out.println(dequeue(dq));
+        System.out.println(dq);
+
+        System.out.println(first(dq));
+        System.out.println(dq);
+
+    }
+
+    public static void enqueue(LinkedList<Integer> ll, int num) {
+        ll.addLast(num);
+    }
+
+    public static int dequeue(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        ll.remove(0);
+        return num;
+    }
+
+    public static int first(LinkedList<Integer> ll) {
+        int num = ll.get(0);
+        return num;
+    }
+}
